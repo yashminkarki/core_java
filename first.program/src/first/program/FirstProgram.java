76817619d51c6a0a80a3856dad4ab6bf8e64b41b
yashminkarki <yashminkarki18@gmail.com>
2021-05-23 01:36:54 -0500

    first.program java files added

---
+++ first.program/src/first/program/FirstProgram.java
@@ -0,0 +1,10 @@
+package first.program;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Yashmin");
+		System.out.println("I have a Bachelor's degree in Computer Science");
+		System.out.print("I am currently staying in Texas");
+}
+}
