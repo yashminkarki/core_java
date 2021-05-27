8bc56cace05d8b29479122a0cb8c37d988d33b80
yashminkarki <yashminkarki18@gmail.com>
2021-05-26 22:58:59 -0500

    Switch case files added

---
+++ selection.statement/src/selection/statement/SwitchCase.java
@@ -0,0 +1,28 @@
+package selection.statement;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		String str = "chd";
+		switch(str) 
+		{
+		default:
+			System.out.println("default case is ready");
+			break;
+		case "BB":
+			System.out.println("Case BB is ready");
+			break;
+		case "AA":
+			System.out.println("Case AA is ready");
+			break;
+		case "chd":
+			System.out.println("Case chd is ready");
+			break;
+		case "ydfhfhh":
+			System.out.println("Case ydfhfhh is ready");
+			break;
+		}
+
+	}
+
+}
