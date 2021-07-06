9f48c4133ba0d4e1dbea7ecabb922755630f3686
yashminkarki <yashminkarki18@gmail.com>
2021-07-06 01:49:46 -0500

    method demo files added

---
+++ method.demo/src/method/demo/MethodDemo.java
@@ -0,0 +1,22 @@
+package method.demo;
+
+public class MethodDemo {
+	
+	static void sum()
+	{
+		System.out.println("Hey. I am sum method");
+		System.out.println("*****************");
+	}
+	static void mul()
+	{
+		System.out.println("Hey. I am multiplication method");
+		System.out.println("*****************");
+	}
+
+	public static void main(String[] args) {
+		sum();
+		mul();
+
+	}
+
+}
---
+++ method.demo/src/method/demo/MethodDemo1.java
@@ -0,0 +1,13 @@
+package method.demo;
+
+public class MethodDemo1 {
+
+	static void helloMethod() {
+		System.out.println("Hello, this is my first method");
+	}
+	public static void main(String[] args) {
+		helloMethod();
+
+	}
+
+}
